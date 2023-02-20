7fccb2da923bb3a5164fe75579127a5b27030f16
Valentin Darricau <darricau.valentin@gmail.com>
2023-02-20 17:28:55 +0700

    Add friend in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +42,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @var User $newFriend
+     */
+    public function addFriend(User $newFriend): void
+    {
+        $this->friends()->save($newFriend);
+        $newFriend->friends()->save($this);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function friends(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'friends', 'friend_connecting_id', 'friend_accepting_connection_id');
+    }
 }
---
+++ database/migrations/2023_02_20_095342_add_friends_relationship_user.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->foreignIdFor(User::class, 'friend_connecting_id');
+            $table->foreignIdFor(User::class, 'friend_accepting_connection_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('friends');
+    }
+};
